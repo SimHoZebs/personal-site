78b39b24a5715172b8e5eb964acbb49fb24c154b
SimHoZebs <simhozebs@gmail.com>
2025-03-08 15:37:48 -0500

    dependency update; tailwind4 migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import figcaptionPlugin from "./src/plugin/FigcaptionPlugin";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), mdx()],
+
   markdown: {
-    rehypePlugins: [figcaptionPlugin],
-    shikiConfig: {
-      theme: "github-dark-dimmed",
-    },
+      rehypePlugins: [figcaptionPlugin],
+      shikiConfig: {
+          theme: "github-dark-dimmed",
+      },
+	},
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
