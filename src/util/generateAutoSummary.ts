bd8b2d6df9cf85d8dbd14eb9f96dca604fe4993b
SimHoZebs <simhozebs@gmail.com>
2025-08-20 14:51:02 -0400

    bring back summaries

--- src/util/generateAutoSummary.ts
+++ src/util/generateAutoSummary.ts
@@ -1,14 +1,7 @@
 const CHAR_PER_MIN = 238;
-const skip = /^[!#>]/; // Avoid callouts, headings, images
+const skip = /^[!#>]|^\\-/; // Avoid callouts, headings, images
 
-export function generateAutoSummary(
-  body: string,
-  description?: string,
-): string {
-  if (description) {
-    return description; // Return frontmatter description if available
-  }
-
+export function generateAutoSummary(body: string): string {
   const paragraphs = body.split("\n");
   let autoSummary = "";
 
@@ -16,14 +9,15 @@ export function generateAutoSummary(
     const paragraph = paragraphs[i].trim();
     if (paragraph.match(skip) || paragraph === "") continue;
 
-    autoSummary = paragraph;
+    autoSummary += paragraph + " ";
     // Strip markdown
     autoSummary = autoSummary.replace(/[*_]/g, "");
     autoSummary = autoSummary.replace(/\[(.*?)\]\(.*?\)/g, "$1"); // Replace links with link text
 
-    if (autoSummary.length > CHAR_PER_MIN)
-      autoSummary = `${autoSummary.slice(0, CHAR_PER_MIN)}...`;
-    break; // Use the first valid paragraph
+    if (autoSummary.length > CHAR_PER_MIN) {
+      autoSummary = `${autoSummary.slice(0, CHAR_PER_MIN)}`;
+      break;
+    }
   }
 
   return autoSummary;
