abe6df5743e94cbc9b5d15eac26ec3ec5d473efb
SimHoZebs <simhozebs@gmail.com>
2025-08-20 00:42:54 -0400

    simplify readTimeEstimation input

--- src/util/readTimeEstimation.ts
+++ src/util/readTimeEstimation.ts
@@ -1,5 +1,7 @@
 const CHAR_PER_MIN = 238;
-const readingMinsEstimation = (contentLength: number) => {
+
+const readingMinsEstimation = (text: string) => {
+  const contentLength = text.split(" ").length;
   return Math.floor(contentLength / CHAR_PER_MIN);
 };
 
